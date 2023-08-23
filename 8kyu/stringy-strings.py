6ddeff18613ebbd0dcb07fef828adefb901777af
mohamed chaabi <chaabi.mohamed@ofppt-edu.ma>
2023-08-23 11:22:33 +0100

    add stringy-strings.py

---
+++ 8kyu/stringy-strings.py
@@ -0,0 +1,20 @@
+import random
+# DESCRIPTION:
+# write me a function stringy that takes a size and returns a string of alternating 1s and 0s.
+
+    # the string should start with a 1.
+
+    # a string with size 6 should return :'101010'.
+
+    # with size 4 should return : '1010'.
+
+    # with size 12 should return : '101010101010'.
+
+# The size will always be positive and will only use whole numbers.
+def stringy(size):
+    result = "1"
+    for i in range(size-1):
+        result += str(random.randint(0, 1))
+    return result
+
+print(stringy(6))
