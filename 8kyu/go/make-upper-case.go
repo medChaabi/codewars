d8e751df6789b6121873a0f8f8f1e813d33bde6b
mohamed chaabi <chaabi.mohamed@ofppt-edu.ma>
2023-09-03 14:41:20 +0100

    add: make-upper-case

---
+++ 8kyu/go/make-upper-case.go
@@ -0,0 +1,11 @@
+// DESCRIPTION:
+// Write a function which converts the input string to uppercase.
+
+package kata
+
+import "strings"
+
+func MakeUpperCase(str string) string {
+    var output string = strings.ToUpper(str)
+	  return output
+}
