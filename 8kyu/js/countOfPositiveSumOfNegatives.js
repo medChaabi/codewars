bd12756d04336a66f1ea5f23d6e9f911720b53b4
mohamed chaabi <chaabi.mohamed@ofppt-edu.ma>
2023-09-11 18:06:20 +0100

    update /js count positives sum negatives

--- 8kyu/js/countOfPositiveSumOfNegatives.js
+++ 8kyu/js/countOfPositiveSumOfNegatives.js
@@ -1,15 +1,16 @@
 function countPositivesSumNegatives(input) {
+  if(input === null || input.filter(n=>n!=0).length==0) return []
   let countP = 0;
   let sumN = 0;
   input.map((num) => {
-    if (num > 0) {
-      countP += 1;
-    } else {
-      sumN += num;
-    }
+    if (num > 0) countP += 1;
+    if (num < 0) sumN += num;
   });
   return [countP, sumN];
 }
 
 const res = countPositivesSumNegatives([1, 2, 3, -10, -11, -1]);
+// const resb = countPositivesSumNegatives([]);
+const resb = countPositivesSumNegatives([]);
 console.log(res);
+console.log(resb);
